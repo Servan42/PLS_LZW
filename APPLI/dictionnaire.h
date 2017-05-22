faa17c514ecda70bc6917b9af51dbb75ce5ae359
tozenda <thom.ozenda@gmail.com>
2017-05-22 16:11:13 +0200

    Dictionnaire.h 1.0

--- APPLI/dictionnaire.h
+++ APPLI/dictionnaire.h
@@ -1,10 +1,14 @@
 #ifndef DICTIONNAIRE_H
 #define DICTIONNAIRE_H
 
-typedef Code int;
+typedef int Code;
 
 void initialiser();
 
-Code Inserer(Code prefixe,Code mono)
+Code Inserer(Code prefixe,Code mono);
+
+uint8_t *CodeVersChaine (Code code, int *longueur);
+
+Code SequenceVersCode (uint8_t *sequence, int longueur);
 
 #endif
