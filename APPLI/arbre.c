67cdae7e6efebb9b9296215cd70046a849a62a95
rdresy <r.dresy@gmail.com>
2017-05-29 17:12:24 +0200

    structure arbre

--- APPLI/arbre.c
+++ APPLI/arbre.c
@@ -1,2 +1,44 @@
-#include arbre.h
+#include "arbre.h"
 
+p_arbre Initialiser_arbre(Code code, p_arbre gauche, p_arbre droite){
+	p_arbre a = malloc(sizeof(arbre));
+	a->code = code;
+	a->gauche = gauche ;
+	a->droite = droite ;
+ 	return a; 
+}
+
+p_arbre inserer_gauche(int code, p_arbre *tree){
+	p_arbre i = Initialiser_arbre( code, (*tree)->gauche, NULL);
+	(*tree)->gauche = i;
+	return tree;
+}
+
+p_arbre inserer_droite(int code, p_arbre *tree){
+	p_arbre i = Initialiser_arbre( code, NULL, (*tree)->droite);
+	(*tree)->droite = i;
+	return tree; 
+}
+
+void print_arbre(p_arbre tree, int niveau ){
+	int i; 
+
+	if (tree==NULL) {printf("erreur lors de l'affichage de l'arbre");}
+
+	else {
+
+		print_arbre (tree->droite,niveau+1) ;
+	
+		for (i = 0; i < niveau; i++)
+		  printf ("\t") ;
+		printf (" %d (%d)\n\n", a->code, niveau) ;
+
+		print_arbre (tree->gauche, niveau+1) ;
+        }
+  return ;
+		
+}
+
+
+	
+	
--- APPLI/arbre.h
+++ APPLI/arbre.h
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include "dictionnaire.h"
+
 //comme en MN
-//Dans notre structure, descendre à gauche reient à changer la lettre de la position courante
+//Dans notre structure, descendre à gauche revient à changer la lettre de la position courante
 //Descendre à droite correspond à avancer d'une lettre et donc à "valider" la précédente
-struct arbre_t{
-	int code; // le code de compression correspondant
+typedef struct arbre_t{
+	Code code; // le code de compression correspondant
 	struct arbre_t * gauche; //suite du mot à gauche
 	struct arbre_t * droite; //suite du mot à droite
 }arbre, *p_arbre;
 
-void imprimer_arbre(p_arbre tree)
+void imprimer_arbre(p_arbre tree);
 
-p_arbre Initialiser_arbre( int code, p_arbre gauche , p_arbre droite ) ;
+p_arbre Initialiser_arbre( Code code, p_arbre gauche , p_arbre droite ) ;
 
 p_arbre inserer_gauche(int code, p_arbre *tree);
 
